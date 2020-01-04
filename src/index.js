95709d8111b5c8618d6968a2c9ff689cc7c10b4e
Vlad Svistielnik <svistv96@gmail.com>
2020-01-04 21:53:35 +0200

    header on mobile complete

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import './assets/scss/layout.scss';
 import './assets/scss/styles.scss';
 
 
+const menu = document.querySelector('.menu');
+const burger = document.getElementById("burgerContainer");
+burger.onclick = function () {
+    this.classList.toggle("change");
+
+    if (menu.classList.contains('openMenu')) {
+        menu.classList.remove('openMenuAnimation');
+        setTimeout(() => {
+            menu.classList.remove('openMenu');
+        }, 1000);
+    } else {
+        menu.classList.add('openMenu');
+        setTimeout(() => {
+            menu.classList.add('openMenuAnimation');
+        }, 20);
+    }
+};
+
